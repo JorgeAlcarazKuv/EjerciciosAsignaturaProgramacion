bfb8d54b4fd5184b2c89e96513691c0f04b29262
Jorge Alcaraz Bravo <jrg.kuv@gmail.com>
2016-10-05 15:29:32 +0200

    añadido comentario

--- Tema3/Ejercicio12.java
+++ Tema3/Ejercicio12.java
@@ -11,6 +11,6 @@ public class Ejercicio12 {
     System.out.println("Qué nota quieres sacar este trismestre?: ");
     double notaDeseada = Double.parseDouble(System.console().readLine());
     System.out.printf("Para tener un %.2f este trimestre necesitas sacar un %.2f ", notaDeseada, 
-    (notaDeseada-primerExamen*0.4)/0.6);
+    (notaDeseada-primerExamen*0.4)/0.6);   //primerExamen*0.4+segundoExamen*0.6=notaDeseada
   }
 }
